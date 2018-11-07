30ec0b1fad20d53c02d6375d9f5cd6418910a5a9
g-0-p-h-e-r <g0pher@tistory.com>
2018-11-07 21:16:02 +0900

    챗봇

--- _chatBot.py
+++ _chatBot.py
@@ -2,9 +2,12 @@
 
 def bot(content):
     msg = {}
-    answer = mean(content)
+    answer = answer(mean(content))
     msg['message'] = {'text': answer }
     return msg
 
 def mean(content):
+    return {'type a':1, 'type b':2}
+
+def answer(meaning):
     return "챗봇 서비스 준비중입니다."
